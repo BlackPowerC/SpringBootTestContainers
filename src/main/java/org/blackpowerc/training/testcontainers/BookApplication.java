971737025fac1b7f2b36fc59ec3aadb3cf986be5
jojo <jordy.fatigba@theopentrade.com>
2025-11-28 11:51:46 +0000

    Create spring boot main.

---
+++ src/main/java/org/blackpowerc/training/testcontainers/BookApplication.java
@@ -0,0 +1,12 @@
+package org.blackpowerc.training.testcontainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(BookApplication.class, args);
+    }
+}
